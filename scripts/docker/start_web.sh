ff24e297d2bf17dba2de6359d1532bfd7edcf5ae
Braintree <code@getbraintree.com>
2015-01-09 23:24:44 +0000

    more docker changes.

--- scripts/docker/start_web.sh
+++ scripts/docker/start_web.sh
@@ -2,10 +2,10 @@ sudo docker run -d --name word_tracker_web_1 \
   --link word_tracker_db:db \
   --publish 4000:4000 \
   --volume /home/goggin/word-tracker:/word-tracker/app \
-  word_tracker_web:v2
+  word_tracker_web:v1
 
 sudo docker run -d --name word_tracker_web_2 \
   --link word_tracker_db:db \
-  --publish 4001:4001 \
+  --publish 4001:4000 \
   --volume /home/goggin/word-tracker:/word-tracker/app \
-  word_tracker_web:v2
+  word_tracker_web:v1
