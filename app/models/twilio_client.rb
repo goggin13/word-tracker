0e375d7a286691213cb4a7824d53c235cbdead01
Goggin <code@getbraintree.com>
2017-12-08 14:30:36 -0600

    add logging for twilio sent

--- app/models/twilio_client.rb
+++ app/models/twilio_client.rb
@@ -1,9 +1,14 @@
 class TwilioClient
   def self.text(message)
-    TWILIO_CLIENT.messages.create(
+    result = TWILIO_CLIENT.messages.create(
       from: TWILIO_FROM_NUMBER,
       to: TWILIO_TO_NUMBER,
       body: message
     )
+
+    message = "Sent text to #{TWILIO_TO_NUMBER}"
+    Rails.logger.info(message)
+
+    result
   end
 end
